eab0847a618bbe6c1f36e4a8b427384947fd5609
Alexander Goncharuk <sashael@ukr.net>
2020-02-28 21:54:39 +0200

    `

--- ado08022020/Form1.Designer.cs
+++ ado08022020/Form1.Designer.cs
@@ -30,9 +30,8 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.cbShowAdmin = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -54,41 +53,36 @@
             this.cbShowAdmin.Text = "Show admins";
             this.cbShowAdmin.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btAdd
             // 
-            this.button1.Location = new System.Drawing.Point(13, 370);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btAdd.BackColor = System.Drawing.Color.PaleGreen;
+            this.btAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btAdd.Location = new System.Drawing.Point(13, 370);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(75, 23);
+            this.btAdd.TabIndex = 2;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = false;
+            this.btAdd.Click += new System.EventHandler(this.BtAdd_Click);
             // 
-            // button2
+            // btDelete
             // 
-            this.button2.Location = new System.Drawing.Point(95, 370);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(177, 370);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btDelete.BackColor = System.Drawing.Color.Tomato;
+            this.btDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btDelete.Location = new System.Drawing.Point(177, 370);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(75, 23);
+            this.btDelete.TabIndex = 4;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(265, 407);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.btAdd);
             this.Controls.Add(this.cbShowAdmin);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
@@ -102,9 +96,8 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.CheckBox cbShowAdmin;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btDelete;
     }
 }
 
--- ado08022020/Form1.cs
+++ ado08022020/Form1.cs
@@ -12,9 +12,28 @@ namespace ado08022020
 {
     public partial class Form1 : Form
     {
+        public List<Employee> Employees { get; set; } = new List<Employee>();
         public Form1()
         {
             InitializeComponent();
+            listBox1.DataSource = ;
+        }
+        private void BtAdd_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(this);
+            form2.ShowDialog();
+        }
+    }
+    public class Employee
+    {
+        public string Login { get; set; }
+        public string Pass { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public bool IsAdmin { get; set; }
+        public override string ToString()
+        {
+            return Login;
         }
     }
 }
